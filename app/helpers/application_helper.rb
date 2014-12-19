ef63c42c20a5a2c510c14a489a7997b3cada1c3a
Andrew Roth <andrewroth@gmail.com>
2014-12-18 19:40:48 -0500

    pull footer properly in production

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -169,4 +169,20 @@ module ApplicationHelper
     end
   end
 
+  def pull_footer
+    if Rails.env.production?
+      uri = URI.parse("https://#{community_host}/footer.html")
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      request = Net::HTTP::Get.new(uri.request_uri)
+
+      response = http.request(request)
+      return response.body.html_safe
+    else
+      Net::HTTP.get(community_host, '/footer.html').html_safe
+    end
+  end
+
 end
