d1bc8eb7f4ff60af25eb5a1c0b3a4a8087167bd1
Andrew Roth <andrewroth@gmail.com>
2014-12-19 19:04:50 -0500

    add todo to make quotes prettier

--- app/assets/javascripts/discourse/lib/quote.js
+++ app/assets/javascripts/discourse/lib/quote.js
@@ -29,6 +29,12 @@ Discourse.Quote = {
 
     //result = "[quote=\"" + post.get('username') + ", post:" + post.get('post_number') + ", topic:" + post.get('topic_id');
     result = "<div><blockquote class='inline_quote'><span class='someonesaid'>" + post.get('username') + "</span><div>" + contents + "</div></blockquote></div><div></div>";
+    /* TODO See if I can make this look prettier at some point... like the <aside> tag below
+
+    [24] pry(main)> PrettyText.markdown(%|[quote="andrewroth, post:1, topic:321, full:true"]\nJust testing some stuff\n[/quote]|)
+        User Load (2.6ms)  SELECT  "users".* FROM "users"  WHERE "users"."username_lower" = 'andrewroth' LIMIT 1
+        => "<aside class=\"quote\" data-post=\"1\" data-topic=\"321\" data-full=\"true\"><div class=\"title\"><div class=\"quote-controls\"></div><img width='20' height='20' src='/user_avatar/localhost/andrewroth/40/383.png' class='avatar'>andrewroth:</div><blockquote><p>Just testing some stuff</p></blockquote></aside>"
+        */
 
     /* Strip the HTML from cooked */
     tmp = document.createElement('div');
