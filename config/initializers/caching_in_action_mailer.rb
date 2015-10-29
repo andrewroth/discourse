120dcdddd20565bd391d12d5d684e521bd500452
Andrew Roth <andrewroth@gmail.com>
2015-10-29 00:36:54 -0400

    try a different approach in sending emails;
    wasn't working in production console on the server

--- config/initializers/caching_in_action_mailer.rb
+++
@@ -1,8 +0,0 @@
-ActionMailer::Base.class_eval do
-  def perform_caching
-    Rails.configuration.action_controller.perform_caching
-  end
-  def controller_name
-    "ActionMailer" # anything, really
-  end
-end
